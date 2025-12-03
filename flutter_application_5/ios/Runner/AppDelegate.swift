2faf7268e352d02a2dc3bedccfa68865fbaeebe0
Fernanda 260504 <20223tn086@utez.edu.mx>
2025-12-02 23:38:14 -0600

    Examen Practico

--- flutter_application_5/ios/Runner/AppDelegate.swift
+++ flutter_application_5/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-      GMSServices.provideAPIKey("API_KEY")   //  tu API Key
+      GMSServices.provideAPIKey("AIzaSyBGunly0ioS2Ex6hzuEUkWNR8E_ALIqS84")   //  tu API Key
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- flutter_application_5/lib/main.dart
+++ flutter_application_5/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'map_page.dart';
+import 'branch_map_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MapPage(),
+      home: BranchMapPage(),
     );
   }
 }
